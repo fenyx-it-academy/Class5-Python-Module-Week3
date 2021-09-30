be324ffa49fc6d52cbf026e37f427934325403ec
Kefe <89358580+KadrEfe@users.noreply.github.com>
2021-09-30 11:34:06 +0200

    name_capitalzie.py
    Bonus question 2.Hackerrank

--- name_capitalize.py
+++ name_capitalize.py
@@ -3,10 +3,10 @@ capital letter in their passports. For example,
 alison heck should be capitalised correctly as Alison Heck."""
 
 
-Given a full name, your task is to capitalize the name appropriately.
+#Given a full name, the task is to capitalize the full name appropriately.
 def name_check():
-  nr_list=[n for n in input("Avoiding repeating(split with comma ',')  :").split(' ')]
-  #given full name will directly go in list(nrlist).T
+  nr_list=[n for n in input("Avoiding repeating(split with a space ' ')  :").split(' ')]
+  #given full name(splitted with" ") will directly go in list(nrlist).
   name=(''.join(nr_list[0])).capitalize()
   surname=(''.join(nr_list[1])).capitalize()
   #due to capitalize() method,we convert first letter of each words to a capital letter.
